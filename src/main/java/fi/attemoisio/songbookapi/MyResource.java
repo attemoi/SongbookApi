4ff01a07212a499dc046aabdcd11cd407be8af15
Atte Moisio <atte.moisio@attemoisio.fi>
2014-09-12 13:56:49 +0300

    Add example Swagger annotations to MyResource

--- src/main/java/fi/attemoisio/songbookapi/MyResource.java
+++ src/main/java/fi/attemoisio/songbookapi/MyResource.java
@@ -5,10 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponses;
+import com.wordnik.swagger.annotations.ApiResponse;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
+@Api(value = "/myresource", description = "Operations about myresource")
+@Produces({"text/plain"})
 public class MyResource {
 
     /**
@@ -18,6 +25,10 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
+    @ApiOperation(value = "Get Hello world message", notes = "More notes about this method")
+    @ApiResponses(value = {
+      @ApiResponse(code = 404, message = "Resource not found") 
+    })
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Hello, Heroku!";

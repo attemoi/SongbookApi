fb91de0d93b80bad933785f73bc39d2d61e255e1
Atte Moisio <atte.moisio@attemoisio.fi>
2014-09-13 20:45:34 +0300

    Create model for songbook.

---
+++ src/main/java/fi/attemoisio/songbookapi/model/Songbook.java
@@ -0,0 +1,57 @@
+package fi.attemoisio.songbookapi.model;
+
+import java.sql.Date;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Songbook")
+public class Songbook {
+	
+	private int id;
+	private int ownerId;
+	private Date releaseDate;
+	private String title;
+	private String description;
+	
+	@XmlElement(name = "id")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@XmlElement(name = "ownerId")
+	public int getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+	
+	@XmlElement(name = "releaseDate")
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	@XmlElement(name = "getTitle")
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@XmlElement(name = "description")
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
